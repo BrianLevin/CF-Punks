b86e5dba948a1c61a34037c17d6921629cf49ad5
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-06-01 14:39:53 -0400

    added logo and search bar

--- cp-punks/src/components/Header.js
+++ cp-punks/src/components/Header.js
@@ -1,13 +1,22 @@
-import React from 'react'
-import './Header.css'
-import punkLogo from  "../assets/header/cryptopunk-logo.png"
- export const Header = () => {
+import React from "react";
+import "./Header.css";
+import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
+
+export const Header = () => {
   return (
-    <div className= "header">
-       <div className = "logoContainer">
-           <img src= {punkLogo} className= "punkLogo" alt = ""/>
-       </div>
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} className="punkLogo" alt="" />
+      </div>
+
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} />
+        </div>
+        <input className="searchInput" placeholder = "Collection, Item or user..." />
+      </div>
     </div>
-  )
-}
-export default Header
+  );
+};
+export default Header;
