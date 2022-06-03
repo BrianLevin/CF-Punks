07435a4b082a68da337829078d98c9f1f36ae7e7
BrianLevin123 <93602751+BrianLevin123@users.noreply.github.com>
2022-06-03 11:02:14 -0400

    made collections cards component

--- cp-punks/src/App.js
+++ cp-punks/src/App.js
@@ -8,9 +8,9 @@ function App() {
     <div className="app">
       <Header />
       <Collectioncard  
-      id ={0}  
+      id = {0}  
       name= {'Bandana Punk'} 
-      traits={[{'value': 7}]}  
+      traits= {[{'value':7}]}  
       image = "https://ipfs.thirdweb.com/ipfs/QmZ5fD3UTRh8ALZCpMdypHkhMQSXyi4yyCz3Ea19kPmtXg/0.jpg"
       />
     </div>
--- cp-punks/src/components/Collectioncard.js
+++ cp-punks/src/components/Collectioncard.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
 import weth from "../assets/weth.png"
+import "./Collectioncard.css"
+
 export const Collectioncard = ({id, name, traits, image}) => {
   return (
     <div className= "collectionCard">
         <img src= {image}  alt= "" />
-        <div className="details"></div>
+        <div className="details">
         <div className="name">
             {name} <div className="id"> .#{id}</div>
+            
             <div className="priceContainer">
                 <img src = {weth} className = "wethImage" />
-                <div className= "price">{traits[0]?.value}</div>
+                <div className="price">{traits[0] ?.value}</div>
+                </div>
             </div>
         </div>
     </div>
